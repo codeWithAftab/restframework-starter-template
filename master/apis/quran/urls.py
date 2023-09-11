a34d713ce3452beab3ed02f3dae324f21dd641ec
Aftab <aftab.ahmad@incupad.com>
2023-09-11 16:09:48 +0530

    quran data

--- master/apis/quran/urls.py
+++ master/apis/quran/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from ..quran import views
 
 urlpatterns = [
-    # path('chapters/', views.QuranChaptersApi.as_view(), name="quranChapters"),
-    # path('chapter/<int:chapter_id>/', views.QuranChaptersApi.as_view(), name="quranChapter"),
-    # path('languages/', views.LanguagesListApi.as_view(), name="language_api"),
-    # path('chapters/search/', views.SearchQuran.as_view(), name="quranChapters"),
-    # path('chapter/<int:chapter_id>/search/', views.SearchQuran.as_view(), name="quranChapter"),
+    path('chapters/', views.QuranChaptersApi.as_view(), name="quranChapters"),
+    path('chapter/<int:chapter_id>/', views.QuranChaptersApi.as_view(), name="quranChapter"),
+    path('languages/', views.LanguagesListApi.as_view(), name="language_api"),
+    path('chapters/search/', views.SearchQuran.as_view(), name="quranChapters"),
+    path('chapter/<int:chapter_id>/search/', views.SearchQuran.as_view(), name="quranChapter"),
 ]
