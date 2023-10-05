2cce68902a872b149b64b4a54e29f6ac680a8d8e
Aftab <aftab.ahmad@incupad.com>
2023-10-05 15:15:50 +0530

    init

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,11 +124,13 @@ AUTH_USER_MODEL = 'account.CustomUser'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn/')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
