2539a1f3fb8939913c9cada758a4848b8ab742c2
Aftab <aftab.ahmad@incupad.com>
2023-10-16 10:58:27 +0530

    remove image size restriction

--- master/forms.py
+++ master/forms.py
@@ -9,17 +9,17 @@ class OnBoardingScreenAdminForm(forms.ModelForm):
         model = OnBoardingScreens
         fields = '__all__'
 
-    def clean(self):
-        cleaned_data = super().clean()
-        image = cleaned_data.get('image')
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     image = cleaned_data.get('image')
 
-        if image:
-             # Read the file content from the in-memory uploaded file
-            file_content = BytesIO(image.read())
-            image = Image.open(file_content)
-            width, height = image.size
-            required_width = 720  
-            required_height = 1280 
+    #     if image:
+    #          # Read the file content from the in-memory uploaded file
+    #         file_content = BytesIO(image.read())
+    #         image = Image.open(file_content)
+    #         width, height = image.size
+    #         required_width = 720  
+    #         required_height = 1280 
 
-            if width != required_width or height != required_height:
-                raise ValidationError("The image should be in WxH 720x1280 dimension.")
+    #         if width != required_width or height != required_height:
+    #             raise ValidationError("The image should be in WxH 720x1280 dimension.")
