633155fe52ee6a6c16c945f1e07b464620d19677
Aftab <aftab.ahmad@incupad.com>
2023-10-19 08:41:26 +0530

    added list of post update api

--- master/apis/posts/staff_views.py
+++ master/apis/posts/staff_views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, UpdateAPIView
 from rest_framework.views import APIView
 from .serializers import PostsSerializer, ReplySerializer, CommentSerializer, TagSerializer, CategorySerializer, PostViewSerializer, IslamicBookSerializer
 from master.models import Category, Reply, Comment, Tag, Post, PostView, IslamicBook
@@ -28,17 +28,39 @@ class GetIslamicPostsAPI(ListAPIView):
         return Post.objects.prefetch_related("views").filter(book__book_id=book_id)
     
 
-class VerifyBookPostsAPI(APIView):
+class VerifyBookPostsAPI(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostsSerializer
+    lookup_field = "id"
+
+    # def get_objects(self):
+    #     queryset = self.get_queryset()
+    #     filtered_queryset = 
+    #     data = self.request.data["data"]
+    #     for post in data:
+
+
     def post(self, request):
         try:
-            post_id = request.data["post_id"]
-            post = Post.objects.get(id=post_id)
-            post.is_verified = True
-            post.save()
-            return Response({"msg":"success", "data": PostsSerializer(post, context={"request":request}).data, })
+            up_posts = request.data["data"]
+            posts = []
+            for up_post in up_posts:
+                print(up_post["id"])
+                post = Post.objects.get(id=up_post["id"])
+                if post.is_verified != up_post["is_verified"]:
+                    post.is_verified = up_post["is_verified"]
+                    post.save()
+
+                posts.append(post)
+            # instance = self.get_object()
+            # print(instance)
+            # serializer = PostsSerializer(instance=instance, data=up_posts, many=True, partial=True)
+            # serializer.update()
+
+            return Response({"msg":"success"})
         
         except KeyError as e:
-            return Response({"code": "missing_post_id", "msg": f"post_id is mandatory.{e}" }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({"code": "missing_data", "msg": f"missing .{e}" }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
         except Exception as e:
             return Response({"code":"post_not_found", "msg": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
